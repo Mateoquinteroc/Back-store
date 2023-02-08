4df76993c90bd255cc2daab6be163e8b299f726a
MateoQuinteroC <mateoquintero05@gmail.com>
2023-02-07 20:50:14 -0500

    cambio entorno db

--- libs/postgres pool.js
+++ libs/postgres pool.js
@@ -2,9 +2,17 @@ const { Pool } = require('pg');
 
 const { config } = require('../config/config');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPasssword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+let URI = ''
+
+if (config.isProd) {
+    URI = config.dbUrl; 
+} else {
+    const USER = encodeURIComponent(config.dbUser);
+    const PASSWORD = encodeURIComponent(config.dbPasssword);
+    const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+
+}
+
 
 
 const pool = new Pool({ connectionString: URI });
