f7e1498d76533388947238521ff1c47bc2a78a1a
MateoQuinteroC <mateoquintero05@gmail.com>
2023-02-07 21:46:41 -0500

    modificacion seeders

--- db/config.js
+++ db/config.js
@@ -1,16 +1,13 @@
 const { config } = require('../config/config');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPasssword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 module.exports = {
     development: {
-        url: URI,
+        url: config.dbUrl,
         dialect: 'postgres',
     },
     production: {
-        url: URI,
+        url: config.dbUrl,
         dialect: 'postgres',
     }
 }
